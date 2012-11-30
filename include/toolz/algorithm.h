b998160d41b7c635520cdbc0cfae5f75fffb0858
Artem Germanov <ru.netz@gmail.com>
2012-11-30 10:54:48 -0800

    binary_find with compare class

--- include/toolz/algorithm.h
+++ include/toolz/algorithm.h
@@ -14,16 +14,5 @@ ForwardIterator binary_find(ForwardIterator first,
     return last;
 };
 
-// Function like binary_search, but returns iterator, not bool
-template <class ForwardIterator, class T, class Compare>
-ForwardIterator binary_find(ForwardIterator first,
-    ForwardIterator last, const T& value, Compare comp)
-{
-    first = std::lower_bound(first, last, value, comp);
-    if (first!=last && !(value < *first))
-        return first; //found
-    return last;
-};
-
 #endif
 
